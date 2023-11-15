bd7e51b338c9cf5bbbb07b8cd58c21070ab203d0
xiaolizihahaha <1848698893@qq.com>
2023-11-15 13:25:24 +0800

    fifth

--- 455.py
+++ 455.py
@@ -5,3 +5,27 @@ class Solution(object):
         :type s: List[int]
         :rtype: int
         """
+        g.sort()
+        s.sort()
+
+        p = 0
+        q = 0
+
+        while p < len(g) and q < len(s):
+            if g[p] <= s[q]:
+                p += 1
+                q += 1
+            else:
+                q += 1
+
+        print(p)
+        return p
+
+if __name__ == '__main__':
+    g = [1, 2, 3]
+    s = [1, 1]
+
+    g = [1, 2]
+    s = [1, 2, 3]
+    solution = Solution()
+    solution.findContentChildren(g,s)
